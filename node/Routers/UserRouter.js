0effbc4c1f416e1040b7e6007411798ce0b21e2d
noa662 <noakrav662@gmail.com>
2025-03-28 00:54:46 +0200

    כמעט גמרתי

--- node/Routers/UserRouter.js
+++ node/Routers/UserRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { addUser, deleteUser, getAllUsers, updateUser, getUser } = require("../Controllers/UserController");
 
 const router = express.Router();
+
 router.get("/", getAllUsers);  // שליפת כל המשתמשים 
 router.get("/:id", getUser);  // שליפת משתמש ספציפי לפי ID
 router.post("/", addUser);  // יצירת משתמש חדש
